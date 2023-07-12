99b8363f58f358469fca2273e08e226782c1d914
JCSnap <e0970167@u.nus.edu>
2023-07-12 20:49:59 +0800

    added bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
   return (
-    <div>
+    <div style={{ height: "1800px" }} className="overflow-y-scroll hero-unit">
+      <div style={{ height: "400px" }} className="bg-black flex-row">
+        <img src="https://picsum.photos/200/300" alt="hero" />
+        <div>
+          <h1>Hero</h1>
+          <p>This is the hero section</p>
+        </div>
+      </div>
       <h1>Home</h1>
+      <p>This is the home page</p>
     </div>
   );
 };
