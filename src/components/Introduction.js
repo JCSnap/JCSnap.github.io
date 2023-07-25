b6d3babf2ac411514dfbac8ae13b2a2404772985
JCSnap <e0970167@u.nus.edu>
2023-07-25 23:06:05 +0800

    turned introduction to a reusable component

---
+++ src/components/Introduction.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div className="bg-black d-flex">
+      <div class="flex-row d-flex" id="introductionContainer">
+        <div className="d-flex flex-column justify-content-center px-3">
+          <h1 className="text-white">Hello</h1>
+          <h2 className="text-white">Justin Cheah Yun Fei 📝</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
+import React from "react";
+import Introduction from "../components/Introduction";
+
 const Home = () => {
   return (
     <div style={{ height: "1800px" }} className="overflow-y-scroll hero-unit">
-      <div style={{ height: "400px" }} className="bg-black flex-row">
-        <img src="https://picsum.photos/200/300" alt="hero" />
-        <div>
-          <h1>Hero</h1>
-          <p>This is the hero section</p>
-        </div>
-      </div>
+      <Introduction />
       <h1>Home</h1>
       <p>This is the home page</p>
     </div>
