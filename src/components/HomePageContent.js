9bfc92c4491cf40366f7870cceaf60c00d77e6a2
JCSnap <e0970167@u.nus.edu>
2023-07-29 22:15:42 +0800

    added dummy text for content

---
+++ src/components/HomePageContent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HomePageContent() {
+  return (
+    <div>
+      <section id="aboutMe" className="text-white">
+        <h1>About Me</h1>
+        <p>
+          I am Justin, currently a year 2 undergraduate from the National
+          University of Singapore taking a double degree in Computer Science and
+          Business Administration.
+        </p>
+      </section>
+      <br />
+      <section id="skills">Skills</section>
+    </div>
+  );
+}
+
+export default HomePageContent;
