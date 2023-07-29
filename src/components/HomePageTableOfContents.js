bb5013ef4998fed55d5b5bcaf008c7c286a8ddcc
JCSnap <e0970167@u.nus.edu>
2023-07-29 22:15:59 +0800

    added tableofcontent dummy

---
+++ src/components/HomePageTableOfContents.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomePageTableOfContents() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#aboutMe">About Me</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        {/* Add more list items for other sections */}
+      </ul>
+    </div>
+  );
+}
+
+export default HomePageTableOfContents;
