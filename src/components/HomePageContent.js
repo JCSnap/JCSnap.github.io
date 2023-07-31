b96b6d32fb18f8abbebf5030bcc8956e55695123
JCSnap <e0970167@u.nus.edu>
2023-07-31 22:28:58 +0800

    minor edit

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -8,7 +8,12 @@ function HomePageContent() {
         <p>
           I am Justin, currently a year 2 undergraduate from the National
           University of Singapore taking a double degree in Computer Science and
-          Business Administration.
+          Business Administration. My interest lies in the field of software
+          engineering, and I particularly enjoy finding ways to integrate new
+          technologies to solve problems. Currently, I am tinkering with the use
+          cases of generative AI. I am also interested in Singapore's startup
+          ecosystem amd I hope to be able to connect with more people in the
+          scene.
         </p>
       </section>
       <br />
