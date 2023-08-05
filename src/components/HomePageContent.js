b2277a8344d28bfc541132ce2c3087a220e02bc1
JCSnap <e0970167@u.nus.edu>
2023-08-06 02:16:07 +0800

    added skills

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -13,8 +13,21 @@ function HomePageContent() {
           technologies to solve problems. Currently, I am tinkering with the use
           cases of generative AI. I am also interested in Singapore's startup
           ecosystem amd I hope to be able to connect with more people in the
-          scene.
+          scene. I've had the opportunity to work on a variety of projects, and
+          also at a startup in an AGILE environment using SCRUM framework.
         </p>
+        <h1>Technical Skills</h1>
+        <p>Programming languages: Python, Javascript, Java, Ruby</p>
+        <p>
+          Frameworks: Reactjs, Nextjs, Ruby on Rails, Nodejs, React Native Expo
+        </p>
+        <p>Tools: Git, Firestore, Firebase Functions, Jest, Pinecone</p>
+        <p>Libraries: Boostrap, TaillwindCSS, Langchain, jQuery</p>
+        <p>
+          Other notable libraries: python-telegram-bot, Selenium, Beautiful
+          Soup, sklearn, pandas, numpy, OpenAI, Stable Diffusion
+        </p>
+        <p>Others: HTML, CSS</p>
       </section>
       <br />
       <section id="skills">Skills</section>
