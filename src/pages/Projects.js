b406a63bda2ef9c1f3930574534c511c8e6a5c7a
JCSnap <e0970167@u.nus.edu>
2023-08-06 12:36:42 +0800

    added base for project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,20 @@
+import React from "react";
+import ProjectPageContent from "../components/ProjectPageContent";
+import Sidebar from "../components/Sidebar";
+
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="overflow-y-scroll hero-unit h-1000 bg-main">
+      <div id="contentContainer" className="d-flex flex-row mt-2">
+        <div className="col-lg-3 col-md-3 d-none d-md-block">
+          <Sidebar />
+        </div>
+        <div className="col-lg-6 col-md-9 col-sm-12">
+          <ProjectPageContent />
+        </div>
+        <div className="col-lg-3 d-lg-block d-none"></div>
+      </div>
+      <h1>Home</h1>
     </div>
   );
 };
