088acb90155e8b19b280b9e17a4b34ba3dd61ae2
JCSnap <e0970167@u.nus.edu>
2023-08-07 22:17:43 +0800

    made table of content more complete

--- src/components/HomePageTableOfContents.js
+++ src/components/HomePageTableOfContents.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 function HomePageTableOfContents() {
+  // Define the sections in an array
+  const sections = [
+    { id: "aboutMe", label: "About Me" },
+    { id: "experiences", label: "Experiences" },
+    { id: "education", label: "Education" },
+    { id: "projects", label: "Projects" },
+    { id: "skills", label: "Skills" },
+    // Add more sections here as needed
+  ];
+
   return (
-    <div>
+    <div id="tableOfContent" className="sticky-top">
+      <div className="text-white px-3">Table of Contents</div>
       <ul>
-        <li>
-          <a href="#aboutMe">About Me</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-        {/* Add more list items for other sections */}
+        {sections.map((section) => (
+          <li key={section.id} className="py-1">
+            <a href={`#${section.id}`}>{section.label}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
