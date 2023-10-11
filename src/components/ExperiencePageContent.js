9b02c90fa122ad349a3839925d73193e1d65a1dd
JCSnap <e0970167@u.nus.edu>
2023-10-11 11:48:31 +0800

    updated experience

--- src/components/ExperiencePageContent.js
+++ src/components/ExperiencePageContent.js
@@ -10,7 +10,9 @@ function ExperiencePageContent({ showContactInfo }) {
         period: "August 2023 - Present",
         location: "Singapore",
         contributions: [
-          "Collaborate with Master Engineers and fellow interns to build Minimum Viable Products (MVPs) for NUS backed startups. Currently building a web application for Comcrop, Singapore's first and only rooftop farming startup using Nextjs, Reactjs, Django, MongoDB."
+          "Partner with Master Engineers and fellow interns to build Minimum Viable Products for NUS backed startups. Currently building a web application for Comcrop, Singapore's first and only rooftop farming startup using Nextjs, Reactjs, Django, MongoDB",
+          "Implement features iteratively with AGILE methodologies based on stakeholder requirements, while ensuring robustness through Jest-based testing",
+          "Design and architected systems from scratch based on technical requirements and user stories, set up pipelines for continuous integration to help streamline testing processes",
         ],
       },
       {
