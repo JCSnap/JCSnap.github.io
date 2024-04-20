ff6298757ac801934afe7ed6bd37087a1c9c1f41
JCSnap <e0970167@u.nus.edu>
2024-04-20 13:05:50 +0800

    add responsiveness for hamburger menu

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -6,6 +6,7 @@ function NavigationBar() {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     const [activeTab, setActiveTab] = useState(pathname);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const tabs = [
         {
@@ -30,14 +31,23 @@ function NavigationBar() {
         },
     ];
 
+    const getLabelFromHref = (href: string) => tabs.find((tab) => tab.href === href)?.label;
+
     return (
-        <nav>
-            <ul className="flex pl-2">
+        <nav className="relative">
+            <button className="text-3xl p-4 md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                &#9776; {/* Hamburger Icon */}
+                <span className="ml-4 text-xl md:hidden">{getLabelFromHref(activeTab)}</span>
+            </button>
+            <ul className={`absolute md:relative bg-white w-full md:flex md:pl-2 ${isMenuOpen ? "block" : "hidden"}`}>
                 {tabs.map((tab) => (
-                    <li key={tab.label} className="pr-4">
+                    <li key={tab.label} className="md:pr-4">
                         <button
-                            onClick={() => navigate(tab.href)}
-                            className={`text-xl ${
+                            onClick={() => {
+                                navigate(tab.href);
+                                setIsMenuOpen(false); // Close menu on click
+                            }}
+                            className={`text-xl block md:inline-block p-4 ${
                                 tab.href === activeTab ? "border-b-2 border-sky-800" : "hover:bg-gray-300"
                             } transition-colors duration-300`}
                         >
